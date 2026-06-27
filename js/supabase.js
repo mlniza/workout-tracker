2a60c84b3c82b664474fff9c0870b24a48dec7af
Maulana Izza <mm.izza08@gmail.com>
2026-06-27 10:28:36 +0700

    cred

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,7 @@
 // js/supabase.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
-const SUPABASE_URL  = 'sb_secret_VFULUOGescQEt_nCya2eow_4rVMXuka'
+const SUPABASE_URL  = 'https://wtvfcmshwenvlgntmthe.supabase.co/rest/v1/'
 const SUPABASE_KEY  = 'https://sb_publishable_2h_ldtFGo6PPXAZwvVqkVw_kf5zcYPb.supabase.co'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
